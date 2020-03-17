7b275a7bb8a0464492ed10ced42fd7f233bcb490
_moniet <moniet@sugoilabs.com>
2020-03-17 21:36:47 +0530

    moved flashcard routes

---
+++ routes/flashcards.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const router = express.Router()
+const fs = require('fs')
+const { promisify } = require('util')
+const readFileAsync = promisify(fs.readFile)
+const writeFileAsync = promisify(fs.writeFile)
+
+router.post('/', async (req, res) => {
+	const { word, definition } = req.body
+	console.log(req.body)
+
+	const entry = req.body
+
+	try {
+		const data = await readFileAsync('./db.json')
+		const json = JSON.parse(data)
+		const newData = { ...entry.data, tags: entry.data.tags.split(',') }
+
+		json.data.push(addTags)
+		const result = await writeFileAsync('./db.json', newData)
+		res.json({ message: 'success', flashcards: newData })
+	} catch (err) {
+		console.log(err)
+	}
+})
+
+router.get('/', async (req, res) => {
+	try {
+		const data = await readFileAsync('./db.json')
+		const json = res.json(JSON.parse(data))
+	} catch (err) {
+		console.log(err)
+	}
+})
+
+module.exports = router
