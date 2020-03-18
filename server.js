d5487aa407bbfd6b335e594173a4da879409fc91
_moniet <moniet@sugoilabs.com>
2020-03-18 18:07:07 +0530

    moved flashcards route to router folder

--- server.js
+++ server.js
@@ -1,51 +1,24 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const fs = require('fs')
-const { promisify } = require('util')
 const path = require('path')
-
-const readFileAsync = promisify(fs.readFile)
-const writeFileAsync = promisify(fs.writeFile)
+const flashcards = require('./routes/flashcards')
 const port = 3002
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
+// middleware
 app.use('/static', express.static(path.join(__dirname, 'public')))
+app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')))
 app.use(express.json())
 app.use(cors())
 
-app.get('/', (req, res) => {
-	res.render('pages/index')
-})
+// static pages
+app.get('/', (req, res) => res.render('pages/index'))
+app.get('/create', (req, res) => res.render('pages/create'))
 
-app.post('/flashcards', async (req, res) => {
-	const { word, definition } = req.body
-	console.log(req.body)
-
-	const entry = req.body
-
-	try {
-		const data = await readFileAsync('./db.json')
-		const json = JSON.parse(data)
-		const newData = { ...entry.data, tags: entry.data.tags.split(',') }
-
-		json.data.push(addTags)
-		const result = await writeFileAsync('./db.json', newData)
-		res.json({ message: 'success', flashcards: newData })
-	} catch (err) {
-		console.log(err)
-	}
-})
-
-app.get('/flashcards', async (req, res) => {
-	try {
-		const data = await readFileAsync('./db.json')
-		const json = res.json(JSON.parse(data))
-	} catch (err) {
-		console.log(err)
-	}
-})
+// api
+app.use('/flashcards', flashcards)
 
 app.listen(port, console.log('listening at ' + port))
