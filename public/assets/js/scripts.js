6842133f9772212ce8a93b20a299acc6071e3468
_moniet <moniet@sugoilabs.com>
2020-03-18 19:25:01 +0530

    added fn to fetch flashcards data

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -12,26 +12,10 @@ function changeTheme() {
 	}
 }
 
-const baseUrl = 'http://localhost:3002'
-
-const loadFlashcards = async () => {
-	const baseUrl = 'http://localhost:3002'
-
-	const res = await fetch(`${baseUrl}/flashcards`)
-
-	if (res.status === 200) {
-		return await res.json()
-	}
-
-	return null
-}
-
-document.addEventListener('DOMContentLoaded', async () => {
+document.addEventListener('DOMContentLoaded', () => {
 	const theme = localStorage.getItem('theme')
 	if (theme === 'dark') body.setAttribute('data-theme', 'dark')
+	themeButton.addEventListener('click', changeTheme)
 
 	body.style.opacity = 1
-
-	const flashcards = await loadFlashcards()
-	themeButton.addEventListener('click', changeTheme)
 })
