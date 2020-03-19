4da45416a39fd537ab6fa2e9a3e49198dcdd5cec
_moniet <moniet@sugoilabs.com>
2020-03-19 21:15:17 +0530

    descending order flashcards

--- routes/flashcards.js
+++ routes/flashcards.js
@@ -4,15 +4,27 @@ const fs = require('fs')
 const { promisify } = require('util')
 const readFileAsync = promisify(fs.readFile)
 const writeFileAsync = promisify(fs.writeFile)
+const jwt = require('jsonwebtoken')
+const PASSWORD = process.env.PASSWORD
+const SECRET = process.env.SECRET
 
 router.post('/', async (req, res) => {
+	const token = req.cookies.access_token
+	const decoded = jwt.decode(token, SECRET)
+
+	if (!token || !decoded || PASSWORD !== decoded.password) return res.status(400).end()
+
 	const { word, definition } = req.body
 	const entry = req.body
 
 	try {
 		const data = await readFileAsync('./db.json')
 		const json = JSON.parse(data)
-		const newData = { ...entry.data, tags: entry.data.tags.split(',') }
+		const newData = {
+			...entry.data,
+			tags: entry.data.tags.split(','),
+			createdAt: new Date()
+		}
 
 		json.data.push(newData)
 
@@ -26,7 +38,9 @@ router.post('/', async (req, res) => {
 router.get('/', async (req, res) => {
 	try {
 		const data = await readFileAsync('./db.json')
-		const json = res.json(JSON.parse(data))
+		const parsed = JSON.parse(data)
+		const newData = { data: parsed.data.reverse() }
+		const json = res.json(newData)
 	} catch (err) {
 		console.log(err)
 	}
