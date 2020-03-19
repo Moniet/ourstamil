f4ae5d33ad8b3745d15b302456d69eaff2e3126a
_moniet <moniet@sugoilabs.com>
2020-03-19 21:08:18 +0530

    mode to production

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,21 @@
 const path = require('path')
 
 module.exports = {
-	entry: './public/assets/js/main.js',
+	mode: 'development',
+	entry: {
+		main: './public/assets/js/main.js',
+		create: './public/assets/js/create-form.js'
+	},
 	output: {
-		filename: 'bundle.js',
+		filename: '[name].js',
 		path: path.resolve(__dirname, 'public/dist')
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader', 'postcss-loader']
+			}
+		]
 	}
 }
