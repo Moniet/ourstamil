119b92625923612089c7049b20cc67f7b39fd10e
_moniet <moniet@sugoilabs.com>
2020-03-19 21:12:06 +0530

    added cors credentials

--- server.js
+++ server.js
@@ -2,22 +2,37 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const path = require('path')
-const flashcards = require('./routes/flashcards')
+const cookieParser = require('cookie-parser')
 const port = 3002
 
+require('dotenv').config()
+
+// controllers
+const flashcards = require('./routes/flashcards')
+const auth = require('./routes/auth')
+
+// config
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
-
-// middleware
 app.use('/static', express.static(path.join(__dirname, 'public')))
 app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')))
+
+// middleware
 app.use(express.json())
+app.use(
+	cors({
+		credentials: true
+	})
+)
+app.use(cookieParser())
 
 // static pages
 app.get('/', (req, res) => res.render('pages/index'))
 app.get('/create', (req, res) => res.render('pages/create'))
+app.get('/login', (req, res) => res.render('pages/login'))
 
 // api
 app.use('/flashcards', flashcards)
+app.use('/auth', auth)
 
 app.listen(port, console.log('listening at ' + port))
