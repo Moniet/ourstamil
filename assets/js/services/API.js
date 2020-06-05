755144b835cb15c26eedd5c101d60e51c3b21294
Moniet <moniet.sawhney@gmail.com>
2020-06-05 11:17:40 +0530

    moved load flashcards to API services

---
+++ assets/js/services/API.js
@@ -0,0 +1,25 @@
+export async function postFlashcard(data) {
+	const options = {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			Accept: 'application/json',
+			credentials: 'true'
+		},
+		body: JSON.stringify(data)
+	}
+
+	const res = await fetch(`/flashcards`, options)
+	if (res.status === 401) return alert("Unauthorized Action. You're not logged in.")
+	if (res.status === 400) return alert("Woops! Could not create Flashcard,,p.")
+	if (!res.ok) return alert("Server May be offline.")
+
+	alert("Yaay! Successfully created the tings!")
+}
+
+export async function loadFlashcards() {
+	const res = await fetch(`/flashcards`)
+
+	if (res.status === 200) return await res.json()
+	return null
+}
